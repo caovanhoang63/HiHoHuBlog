eaed0fe14b47548d7136b3cddac5b7057cd5a726
nhdhieuu <22520439@gm.uit.edu.vn>
2024-10-05 13:26:31 +0700

    add Startup.cs

--- Program.cs
+++ Program.cs
@@ -1,35 +1,14 @@
-using HiHoHuBlog.Components;
-using HiHoHuBlog.Modules.User.Biz;
-using HiHoHuBlog.Modules.User.Data;
+using HiHoHuBlog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
-
-
-builder.Services.AddScoped<IUserSignUpStore, DummyData>();
-builder.Services.AddScoped<IUserSignUpBiz,UserSignUpBiz>();
+// Create an instance of Startup and call ConfigureServices
+var startup = new Startup();
+startup.ConfigureServices(builder.Services);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-app.UseAntiforgery();
-
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
-
+// Call the Configure method from Startup
+startup.Configure(app);
 
 app.Run();
--- Startup.cs
+++ Startup.cs
@@ -1,17 +1,35 @@
+using HiHoHuBlog.Components;
+using HiHoHuBlog.Modules.User.Biz;
+using HiHoHuBlog.Modules.User.Data;
+
 namespace HiHoHuBlog;
 
 public class Startup
 {
-    public Startup(IConfiguration configuration)
-    {
-        Configuration = configuration;
-    }
-
-    public IConfiguration Configuration { get; }
-
     public void ConfigureServices(IServiceCollection services)
     {
-        services.AddSingleton<IConfiguration>(Configuration);
+        // Register your services here
+        services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+
+        services.AddScoped<IUserSignUpStore, DummyData>();
+        services.AddScoped<IUserSignUpBiz, UserSignUpBiz>();
     }
 
+    public void Configure(WebApplication app)
+    {
+        // Configure the HTTP request pipeline here
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error", createScopeForErrors: true);
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseAntiforgery();
+
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
+    }
 }
