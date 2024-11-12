5c2d24d00a57e170fea43fd6f2dc79b3e23f602b
hoangcao <22520457@gm.uit.edu.vn>
2024-11-13 04:13:49 +0700

    update admin

--- Utils/Paging.cs
+++ Utils/Paging.cs
@@ -2,8 +2,16 @@ namespace HiHoHuBlog.Utils;
 
 public class Paging
 {
-    int Cursor { get; set; }
-    int NextCursor { get; set; }
-    int PageSize { get; set; }
-    int Total { get; set;  }
+    public Paging(int cursor, int nextCursor, int pageSize, int total)
+    {
+        Cursor = cursor;
+        NextCursor = nextCursor;
+        PageSize = pageSize;
+        Total = total;
+    }
+
+    public int Cursor { get; set; }
+    public int NextCursor { get; set; }
+    public int PageSize { get; set; }
+    public int Total { get; set;  }
 }
