294cf8f63efb9962f4fb2df780984ac9bdc735a8
nhdhieuu <22520439@gm.uit.edu.vn>
2024-11-20 00:26:10 +0700

    Add: Search Tag Services

---
+++ Modules/Search/Service/Implementation/SerchTagService.cs
@@ -0,0 +1,29 @@
+﻿using HiHoHuBlog.Modules.Search.Entity;
+using HiHoHuBlog.Modules.Search.Repository.Interface;
+using HiHoHuBlog.Modules.Search.Service.Interface;
+using HiHoHuBlog.Utils;
+using Quartz.Util;
+
+namespace HiHoHuBlog.Modules.Search.Service.Implementation;
+
+public class SearchTagService(ISearchTagRepository searchTagRepository) : ISearchTagService
+{
+    private readonly ISearchTagRepository _searchTagRepository = searchTagRepository;
+
+    public async Task<Result<IEnumerable<TagSearchDoc>?, Err>> SearchTags(string arg, Paging paging)
+    {
+        if (arg.IsNullOrWhiteSpace())
+        {
+            return Result<IEnumerable<TagSearchDoc>?, Err>.Ok(null);
+        }
+        
+        var r = await _searchTagRepository.SearchTags(arg, paging);
+
+        if (!r.IsOk)
+        {
+            return Result<IEnumerable<TagSearchDoc>?, Err>.Err(r.Error);
+        }
+
+        return r;
+    }
+}
