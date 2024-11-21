904c0f949a7c0a9930c64f182322247817d37969
BuiTrongHoangHuy <22520533@gm.uit.edu.vn>
2024-11-21 17:07:03 +0700

    add get first image

---
+++ Utils/GetThumbnail.cs
@@ -0,0 +1,27 @@
+﻿using HtmlAgilityPack;
+
+namespace HiHoHuBlog.Utils;
+
+public static class GetThumbnail
+{
+    public static async Task<Image> GetThumbnailFromHtml(string htmlContent)
+    {
+        var doc = new HtmlDocument();
+        doc.LoadHtml(htmlContent);
+
+        var imgNode = doc.DocumentNode.SelectSingleNode("//img");
+        if (imgNode != null)
+        {
+            var thumbnailUrl = imgNode.GetAttributeValue("src", null);
+            if (!string.IsNullOrEmpty(thumbnailUrl))
+            {
+                return new Image
+                {
+                    Url = thumbnailUrl,
+                    // Set other Image properties as needed
+                };
+            }
+        }
+        return null;
+    }
+}
