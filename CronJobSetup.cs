83739982a455fa606ebb7b9414008001eaca1696
hoangcao <22520457@gm.uit.edu.vn>
2024-11-26 19:39:11 +0700

    migrate tag and user

--- CronJobSetup.cs
+++ CronJobSetup.cs
@@ -9,14 +9,29 @@ public class CronJobSetup
     {
         service.AddQuartz(q =>
         {
-            var jobKey = new JobKey("MigrateBlog");
-            q.AddJob<MigrateBlog>(opts => opts.WithIdentity(jobKey));
-
+            var migrateBlogKey = new JobKey("MigrateBlog");
+            q.AddJob<MigrateBlog>(opts => opts.WithIdentity(migrateBlogKey));
             q.AddTrigger(opts => opts
-                .ForJob(jobKey) 
-                .WithIdentity("ExampleJob-trigger")
+                .ForJob(migrateBlogKey) 
+                .WithIdentity("MigrateBlogJob-trigger")
                 .WithCronSchedule("0 0 * * * ?")); 
+            
+            var migrateTagKey = new JobKey("MigrateTag");
+            q.AddJob<MigrateTag>(opts => opts.WithIdentity(migrateTagKey));
+            q.AddTrigger(opts => opts
+                .ForJob(migrateTagKey) 
+                .WithIdentity("MigrateTagJob-trigger")
+                .WithCronSchedule("0 0 * * * ?")); 
+            
+            var migrateUserKey = new JobKey("MigrateUser");
+            q.AddJob<MigrateUser>(opts => opts.WithIdentity(migrateUserKey));
+            q.AddTrigger(opts => opts
+                .ForJob(migrateUserKey) 
+                .WithIdentity("MigrateUserJob-trigger")
+                .WithCronSchedule("0 0 * * * ?")); 
+            
         });
+        
         service.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
     }
     
