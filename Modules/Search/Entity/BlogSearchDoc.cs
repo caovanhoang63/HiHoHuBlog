f1d12587014fb6647d445a3a265ae786b42ada08
nhdhieuu <22520439@gm.uit.edu.vn>
2024-12-01 11:35:34 +0700

    Fix: Refractor UI mapping

--- Modules/Search/Entity/BlogSearchDoc.cs
+++ Modules/Search/Entity/BlogSearchDoc.cs
@@ -17,6 +17,7 @@ public class BlogSearchDoc
     public int MinToRead { get; set; }
     public int TotalView { get; set; } = 0;
     public int TotalMark { get; set; } = 0;
+    public int TotalComment { get; set; } = 0;
     public int TotalLike { get; set; } = 0;
     public int Status { get; set; } = 1;
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
