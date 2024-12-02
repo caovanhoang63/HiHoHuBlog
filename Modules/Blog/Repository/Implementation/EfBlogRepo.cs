94fc8300b7303821f48e65c5c94652f804a446bb
nhdhieuu <22520439@gm.uit.edu.vn>
2024-12-02 14:54:54 +0700

    Fix: update Like, Comment, Follow in Repo layer

--- Modules/Blog/Repository/Implementation/EfBlogRepo.cs
+++ Modules/Blog/Repository/Implementation/EfBlogRepo.cs
@@ -323,6 +323,7 @@ public class EfBlogRepo(IMapper mapper, ApplicationDbContext context) : IBlogRep
                 UserId = userId
             });
             await context.SaveChangesAsync();
+            await UpdateTotalLikes(blogId);
             return Result<Unit, Err>.Ok(new Unit());
         }
         catch (Exception e)
@@ -340,6 +341,7 @@ public class EfBlogRepo(IMapper mapper, ApplicationDbContext context) : IBlogRep
                 .FirstOrDefaultAsync(ulb => ulb.UserId == userId && ulb.BlogId == blogId);
             if (entity != null) context.UserLikeBlogs.Remove(entity);
             await context.SaveChangesAsync();
+            await UpdateTotalLikes(blogId);
             return Result<Unit, Err>.Ok(new Unit());
         }
         catch (Exception e)
@@ -394,7 +396,6 @@ public class EfBlogRepo(IMapper mapper, ApplicationDbContext context) : IBlogRep
     {
         try
         {            
-
             await context.UserCommentBlogs.AddAsync(new UserCommentBlog
             {
                 BlogId = blogId,
@@ -402,6 +403,7 @@ public class EfBlogRepo(IMapper mapper, ApplicationDbContext context) : IBlogRep
                 Content = content
             });
             await context.SaveChangesAsync();
+            await UpdateTotalComments(blogId);
             return Result<Unit, Err>.Ok(new Unit());
         }
         catch (Exception e)
