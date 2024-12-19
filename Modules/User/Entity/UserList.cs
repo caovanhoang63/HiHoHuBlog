635ed68793804393ef90f81bc58716f1e25f5ce3
hoangcao <22520457@gm.uit.edu.vn>
2024-12-19 17:32:33 +0700

    add: show following in profile page

--- Modules/User/Entity/UserList.cs
+++ Modules/User/Entity/UserList.cs
@@ -1,4 +1,6 @@
-﻿namespace HiHoHuBlog.Modules.User.Entity;
+﻿using HiHoHuBlog.Utils;
+
+namespace HiHoHuBlog.Modules.User.Entity;
 
 public class UserList
 {
@@ -7,7 +9,11 @@ public class UserList
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
     public string? Phone { get; set; }
+    public string UserName { get; set; }
+
     public string? Role { get; set; }
+    public Image? Avatar { get; set; }   // JSON type mapped to JsonDocument in C#
+
     public int Status { get; set; } = 1;
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
--- Modules/User/Service/Interface/IUserProfileService.cs
+++ Modules/User/Service/Interface/IUserProfileService.cs
@@ -6,4 +6,6 @@ namespace HiHoHuBlog.Modules.User.Service.Interface;
 public interface IUserProfileService
 {
     Task<Result<UserProfile?, Err>> GetUserProfile(string userName);
+    Task<Result<IEnumerable<UserList>?,Err>> GetFollowers(int userId,Paging paging);
+    Task<Result<IEnumerable<UserList>?,Err>> GetFollowings(int userId,Paging paging);
 }
